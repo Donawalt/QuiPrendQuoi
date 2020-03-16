c78fed6ae9d6769284d8da17fe1fcd4b32611053
Donaël Walt <donael.walter@hotmail.com>
2020-03-16 16:24:23 +0100

    récupération param post

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-    res.render('index', { title: 'Qui prend quoi ?' });
-  });
+app.set("view engine", "pug");
+
+app.get("/", function(req, res) {
+  res.render("index", { title: "Qui prend quoi ?" });
+});
+
+app.post("/party", function(req, res) {
+    console.log(req.body);
+  res.send("Post ok !")
+});
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
