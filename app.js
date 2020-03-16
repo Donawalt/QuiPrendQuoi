c2f13949c59467a314ada73a298004d415be76eb
Donaël Walt <donael.walter@hotmail.com>
2020-03-16 16:07:23 +0100

    add pug

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 const dotenv = require('dotenv').config();
 
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-  res.send('Qui prend quoi ? ');
-});
+    res.render('index');
+  });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
