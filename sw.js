2b5b2d07450d0a05fed8dc11ed07c5b293d2cc79
Donawalt <donael.walter@hotmail.com>
2020-03-18 20:19:17 +0100

    Stocker les pages événement

--- sw.js
+++ sw.js
@@ -1,17 +1,39 @@
+function isPartyPage(url) {
+  return /party\/[a-zA-Z0-9]*$/.test(url);
+}
+
 addEventListener("install", event => {
   console.log("Hello from the service worker");
   event.waitUntil(
-    caches.open('offline').then((cache) => {
-      cache.add('offline.html');
+    caches.open("offline").then(cache => {
+      cache.add("offline.html");
     })
   );
 });
 
 addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match('offline.html')),
-      );
-    if (event.request.headers.get("Accept").includes("text/html")) {
-        event.respondWith(fetch(event.request));
-    }
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        if (isPartyPage(event.request.url)) {
+          const copy = res.clone();
+          caches.open("parties").then(cache => cache.put(event.request, copy));
+          return res;
+        } else {
+          return res;
+        }
+      })
+      .catch(() => {
+        if (isPartyPage(event.request.url)) {
+          return caches
+            .match(event.request)
+            .catch(err => caches.match("offline.html"));
+        } else {
+          return caches.match("offline.html");
+        }
+      })
+  );
+  if (event.request.headers.get("Accept").includes("text/html")) {
+    event.respondWith(fetch(event.request));
+  }
 });
